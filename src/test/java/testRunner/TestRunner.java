3dd59423c98366e0ee7b9d8f251cce10b26caea7
hasibrahat10 <hasibrahat10@gmail.com>
2021-03-17 17:41:44 +0600

    update

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -7,7 +7,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features"
-        , glue = {"features/steps"}
+        , glue = {"features/steps"},
+        monochrome = true,
+        plugin = {"pretty", "html:test-output"}
+
 )
 
 public class TestRunner {
